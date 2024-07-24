70b2ff65b8c33b6262d8894d599c491c185a6eec
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2024-07-24 02:34:57 +0200

    Pico CSS & Webpack Encore installed, Front-End integration

--- SimpleQuestion/src/Controller/HomeController.php
+++ SimpleQuestion/src/Controller/HomeController.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Answer;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
